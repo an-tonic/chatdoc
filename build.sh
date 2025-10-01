ee8bf86aeb849b6560c96f7f325d4df187691b78
serdy <aserdyukov@uclan.ac.uk>
2025-10-02 01:16:37 +0300

    Build and run configurations.

---
+++ build.sh
@@ -0,0 +1,11 @@
+adb connect 192.168.1.38:5555
+
+npx react-native bundle \
+  --platform android \
+  --dev false \
+  --entry-file index.js \
+  --bundle-output android/app/src/main/assets/index.android.bundle \
+  --assets-dest android/app/src/main/res/
+
+
+npx react-native run-android
---
+++ run.sh
@@ -0,0 +1,1 @@
+npx react-native start --host 192.168.1.190
